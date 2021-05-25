507187cf54b051bc517ab557682a8bf201a4ddcd
PabloGithubDesktop <pablo@githubdesktop.com>
2021-05-25 13:37:21 +0100

    token

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 let findVersions = require('../lib/findVersions');
 
 let connection = {
-    token: '00D300000000FHi!AQsAQGs2zbatIN9iWzOqzSvKfgG65XUwibUIjyozvufydxFxkHu6H04BC9IIK8xiPFch7DBR_22vi214emINu33L6lup22.J',
-    url:'https://guidewire.my.salesforce.com',
+    token: '00D300000000FHi!gsgsgs.J',
+    url:'https://anything.my.salesforce.com',
     apiVersion:'50.0',
     apiThreshold:20
 };
